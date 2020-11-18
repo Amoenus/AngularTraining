f8edf3ab6b7710984ccbfe5578051d1adee92bc8
Amoenus <motivated.it@gmail.com>
2020-11-18 04:23:14 +0200

    Fixed tests

--- BlogPostViewer/src/app/user/login/login.component.spec.ts
+++ BlogPostViewer/src/app/user/login/login.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { MaterialModule } from 'src/app/shared/material/material.module';
 
 import { LoginComponent } from './login.component';
@@ -17,8 +18,9 @@ describe('LoginComponent', () => {
         RouterTestingModule.withRoutes([]),
         FormsModule,
         MaterialModule,
-        BrowserAnimationsModule
-      ],
+        BrowserAnimationsModule,
+        ],
+        providers: [provideMockStore({})]
     })
       .compileComponents();
   }));
