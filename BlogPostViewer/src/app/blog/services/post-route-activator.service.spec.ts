cfa70fc16d222151854d6af437fa6a24c1c65f8d
Amoenus <motivated.it@gmail.com>
2020-11-20 16:19:48 +0200

    Unit test clenup

--- BlogPostViewer/src/app/blog/services/post-route-activator.service.spec.ts
+++ BlogPostViewer/src/app/blog/services/post-route-activator.service.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { PostRouteActivator } from './post-route-activator.service';
 
@@ -6,7 +8,9 @@ describe('PostRouteActivator', () => {
   let service: PostRouteActivator;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule,
+      RouterTestingModule
+    ] });
     service = TestBed.inject(PostRouteActivator);
   });
 
